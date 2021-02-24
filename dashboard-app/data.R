81e0afc2dc84a1d7d88f0771cbfee133c33b1b2e
bernhard.findeiss@interface-ag.de <bernhard.findeiss@interface-ag.de>
2021-02-24 11:17:55 +0100

    Aufräumen und nicht benötigten Quellcode entfernen

--- dashboard-app/data.R
+++ dashboard-app/data.R
@@ -1,118 +1,75 @@
-extract_final_data <- function(old_data, new_data){
-	
-	data_before_sep <- read.csv(old_data)
-	data_before_sep$date <- as.Date(data_before_sep$date, "%Y-%m-%d")
-	
-	#Berechnung der geschlossenen Fälle
-	
-	closed_per_date <- as.data.frame(table(new_data$abgeschlossen))
-	closed_per_date$Var1 <- as.Date(closed_per_date$Var1, "%Y-%m-%d")
-	closed_per_date <- closed_per_date[closed_per_date$Var1 > "2020-08-31",]
-	
-	closed_cases_end_august <- 641 +3 #drei Personen von außerhalb des Landkreises wurden fälschlicherweise mitgeführt. Sie werden als "genesen" geführt.
-	closed_per_date$cumulative <- cumsum(closed_per_date$Freq) + closed_cases_end_august
-	# Hier kontrolle einbauen!
-	
-	#Berechnung der Todesfälle
-	
-	dead_per_date <- as.data.frame(table(new_data$verstorben))
-	dead_per_date$Var1 <- as.Date(dead_per_date$Var1, "%Y-%m-%d")
-	dead_per_date <- dead_per_date[dead_per_date$Var1 > "2020-08-31",]
-	dead_end_august <- 10
-	dead_per_date$cumulative <- cumsum(dead_per_date$Freq) + dead_end_august
-	
-	#Hier Kontrolle einbauen!
-	
-	#Berechnung der neuen Fälle
-	
-	new_cases <- as.data.frame(table(new_data$Datum)) #Summiert wieviele Fälle an einem Tag aufkamen
-	colnames(new_cases) <- c("date", "new_cases")
-	new_cases$date <- as.Date(new_cases$date, "%Y-%m-%d")
-	
-	#Berechnung der 7-Tage Inzidenz
-	#new_cases$incidence <- c(rep.int(0,6) ,rolling_sum(new_cases$new_cases))
-	
-	data_after_sep <- data.frame(
-		"date" = seq(as.Date("2020-09-01"), max(new_data$Datum, na.rm = TRUE), "days"),
-		"total_cases" = NA,
-		"active_cases" = NA,
-		"closed_cases" = NA,
-		"deaths" = NA,
-		"new_cases" = NA,
-		"incidence" = NA
-		
-	)
-	
-	for (date in data_after_sep$date){
-		
-		case_nr <- max(new_data$Fallnummer[new_data$Datum <= date], na.rm = TRUE)
-		closed <- closed_per_date$cumulative[closed_per_date$Var1 == max(closed_per_date$Var1[closed_per_date$Var1 <= date])]
-		new <- new_cases$new_cases[new_cases$date == date]
-		if(length(new) == 0){
-			new <- 0
-		}
-		
-		if(length(dead_per_date$cumulative) == 0){
-			dead <- 10
-			
-		} else if(length(dead_per_date$cumulative[dead_per_date$Var1 <= date]) == 0) {
-			dead <- 10
-			
-		} else {
-			dead <- tail(dead_per_date$cumulative[dead_per_date$Var1 <= date], n=1)
-			print(dead)
-		}
-		
-		data_after_sep$total_cases[data_after_sep$date == date] <- case_nr
-		data_after_sep$closed_cases[data_after_sep$date == date] <- closed
-		data_after_sep$deaths[data_after_sep$date == date] <- dead
-		data_after_sep$new_cases[data_after_sep$date == date] <- new
-		data_after_sep$active_cases <- data_after_sep$total_cases - data_after_sep$closed_cases - data_after_sep$deaths
-		
-	}
-	
-	population <- 100010
-	data_after_sep$incidence <- round((c(rep.int(0,6) ,rolling_sum(data_after_sep$new_cases))/population)*100000, 0)
-	data_output <- rbind(data_before_sep, data_after_sep)
-	
-	#Übergang bei der Inzidenz schaffen
-	
-	uebergang <- rolling_sum(data_output$new_cases[data_output$date > "2020-08-20"  & data_output$date < "2020-09-10"])
-	
-	data_output$incidence[data_output$date > (as.Date("2020-08-20")+6) & data_output$date < "2020-09-10"] <-  uebergang
-	
-	return(data_output)
+
+## Extraction of the current data from the raw data file
+extract_current_data <- function(ags="09182"){
+  data <- fromJSON(file = paste0("https://api.corona-zahlen.org/districts/", ags))
+  
+  current_data<- as.data.frame(data$data[[ags]])
+  current_data_meta <- as.data.frame(data$meta)
+  
+  current_data<- as.data.frame(data$data[[ags]])
+  current_data_meta <- as.data.frame(data$meta)
+  
+  call_date <- as.POSIXct(current_data_meta$lastCheckedForUpdate, format("%Y-%m-%dT%H:%M:%S"), tz ="UTC")
+  call_date <- call_date + 1*60*60 #um die Stunde Zeitverschiebung einzubeziehen
+  
+  data_date <- as.POSIXct(current_data_meta$lastUpdate, format("%Y-%m-%dT%H:%M:%S"), tz ="UTC")
+  
+  current_data_out <- data.frame(
+    "data_date" = data_date,
+    "call_date" = call_date,
+    "total_cases" = as.integer(current_data$cases),
+    "active_cases" = as.integer(current_data$cases - current_data$recovered - current_data$deaths),
+    "closed_cases" = as.integer(current_data$recovered),
+    "deaths" = as.integer(current_data$deaths),
+    "new_cases" = as.integer(current_data$delta.cases),
+    "new_deaths" = as.integer(current_data$delta.deaths),
+    "new_closed" = as.integer(current_data$delta.recovered),
+    "incidence" = as.integer(round(current_data$weekIncidence))
+  )
+  
+  return(current_data_out)
 }
 
-extract_active_locations <- function(raw_data){
-	active_locations <- raw_data$Postleitzahl[raw_data$Status == 1] #Plz aller aktiven Fälle
-	location_summary <- data.frame(table(active_locations))
-	location_summary$active_locations <- as.integer(as.character(location_summary$active_locations))
-	
-	final_location_data <- read.csv("plz_ref.csv") #Referenztabelle der Postleitzahlen
-	final_location_data$cases <- NA
-	
-	
-	for (plz in final_location_data$PLZ){
-		if(length(location_summary$Freq[location_summary$active_location == plz]) == 0){
-			final_location_data$cases[final_location_data$PLZ == plz] <- 0L
-		} else {
-			final_location_data$cases[final_location_data$PLZ == plz] <- location_summary$Freq[location_summary$active_location == plz] 
-		}
-	}
-	
-	if(sum(location_summary$Freq) != sum(final_location_data$cases)){
-		# check which zip codes outside the region were provided
-		not_considered <- location_summary$active_locations[!location_summary$active_locations %in% final_location_data$PLZ]
-		difference <- sum(location_summary$Freq) - sum(final_location_data$cases)
-		warning(paste0("Achtung! Es konnten "), difference, " Fällen keine Gemeinde im Landkreis zugeordnet werden. Die entsprechenen Postleitzahlen lauten: ", paste(shQuote(not_considered), collapse=", "))
-	}
-	
-	final_location_data$Datum <- replicate(nrow(final_location_data), format(max(raw_data$Datum, na.rm = TRUE)), "%d.%m.%Y")
-	return(final_location_data)
+extract_short_plot_data <- function(ags="09182"){
+  raw_incidence14 <- fromJSON(file = paste0("https://api.corona-zahlen.org/districts/", ags, "/history/incidence/14"), method='C')
+  raw_cases14 <- fromJSON(file = paste0("https://api.corona-zahlen.org/districts/", ags, "/history/cases/14"), method='C')
+  
+  #Abrufzeiten ziehen
+  raw_incidence14_date <- raw_incidence14$meta$lastUpdate
+  raw_cases14_date <- raw_cases14$meta$lastUpdate
+  
+  if(raw_cases14_date == raw_incidence14_date){
+    data_date <- as.POSIXct(raw_incidence14_date, format("%Y-%m-%dT%H:%M:%S"), tz ="UTC")
+  } else {
+    data_date <- NA
+  }
+  
+  call_date <- as.POSIXct(raw_cases14$meta$lastCheckedForUpdate, format("%Y-%m-%dT%H:%M:%S"), tz ="UTC")
+  call_date <- call_date + 1*60*60 #um die Stunde Zeitverschiebung einzubeziehen
+  
+  #Aus dem RKI-Datensatz die richtigen Daten herauslesen
+  incidence14 <- raw_incidence14$data[[ags]]$history
+  incidence14 <- ldply(incidence14, data.frame)
+  incidence14$weekIncidence <- round(incidence14$weekIncidence,0)
+  incidence14$date <- as.Date(incidence14$date,"%Y-%m-%d")
+  
+  
+  cases14<- raw_cases14$data[[ags]]$history
+  cases14 <- ldply(cases14, data.frame)
+  cases14$date <- as.Date(cases14$date,"%Y-%m-%d")
+  
+  # Daten so herrichten, dass sie mit unseren üblichen Plots funktionieren
+  colnames(incidence14) <- c('7-Tage-Inzidenz','Datum')
+  colnames(cases14) <- c('Neuinfektionen','Datum')
+  
+  short_plot_dataframe <- merge(incidence14, cases14, by='Datum')
+  short_plot_dataframe <- melt(short_plot_dataframe, id.vars=c("Datum"), na.rm = TRUE)
+  
+  output <- list(
+    "data" = short_plot_dataframe,
+    "data_date" = data_date,
+    "call_date" = call_date
+    
+  )
+  return(output)
 }
-
-reduce_rows <-function(dataframe, n){
-	dataframe[(seq(n,to=nrow(dataframe),by=n)),]
-}
-
--- dashboard-app/server.R
+++ dashboard-app/server.R
@@ -26,40 +26,6 @@ shinyServer(function(input, output) {
     
     #############################################
     #
-    # Daten für "Werte über die Zeit" und "Kurzfristige Entwicklung" berechnen
-    #
-    #############################################
-    
-    get_summary_data <- reactive({
-      
-        new_data <- read_data() # Daten einlesen
-        if (is.null(new_data))
-            return(NULL)
-        
-        new_data <- new_data[!is.na(new_data$Datum),] #Extrahiere Datensätze mit einem Datum
-        new_data$Datum <- as.Date(new_data$Datum, "%Y-%m-%d") #Umwandeln in Datetime Objekt
-        new_data$abgeschlossen <- as.Date(as.numeric(new_data$abgeschlossen), origin = "1899-12-30") #Umwandeln in Datetime Objekt
-        new_data$verstorben <- as.Date(as.numeric(new_data$verstorben), origin = "1899-12-30") #Umwandeln in Datetime Objekt
-        
-        # Den kompletten Datensatz einlesen und mit den Daten aus der Zeit vor September kombinieren
-        data <- extract_final_data("data_before_september.csv", new_data)
-
-        # Die Spaltennamen umbenennen
-        colnames(data) <- c("Datum", "Fälle", "Aktive Fälle", "Genesen", "Todesfälle", "Neuinfektionen", "7-Tage-Inzidenz")
-        # Zusätzlich zu den normalen Daten auch noch einen Datensatz mit "Melted" Daten erstellen
-        data_melted <- melt(data, id.vars=c("Datum"), na.rm = TRUE)
-        data_melted$Datum <- as.Date(data_melted$Datum, "%d.%m.%Y") #Umwandeln in Datetime Objekt
-        
-        # Output-Objekt enthält sowohl normale, aus auch melted-Daten
-        output <- list(
-            "normal" = data,
-            "melted" = data_melted)
-        
-        return(output)
-    })
-    
-    #############################################
-    #
     # Daten für "Neue Zahlen" berechnen
     #
     #############################################
@@ -102,71 +68,7 @@ shinyServer(function(input, output) {
         data <- get_current_data()
         create_daily_summary(data, background)
     }))
-    
-    #############################################
-    #
-    # Output "Daten Gemeinden" darstellen
-    #
-    #############################################
-    output$location_table <- renderTable({
-        location_data <- get_location_data()
-        if(is.null(location_data))
-            return('Keine Daten. Bitte Datensatz in "Tägliches Update" hochladen')
-        
-        if(!is.null(location_data) & input$sort_alphabetically){
-            location_data <- location_data[order(location_data$Gemeinde),]
-        }
-        
-        print(sum(location_data$cases))
-        #return data without "searchString", "lat" and "long" column" for display
-        subset(location_data, select=-c(searchString, lat, long))
-    })
-    
-    #############################################
-    #
-    # Output "Zusammenfassung" darstellen
-    #
-    #############################################    
-    output$summary_table <- DT::renderDataTable({
-        summary <- get_summary_data()$normal
-        if(is.null(summary))
-            return(NULL)
-        if (input$reverse_summary){
-            summary <- summary[order(nrow(summary):1),] # inverts the list
-        }
-        
-        return(summary)
-        
-    })
-
-    #############################################
-    #
-    # Output "Kartendarstellung" darstellen
-    #
-    #############################################
-    output$map_plot <- renderPlot({
-      landkreis_map()
-    })
-    
-    landkreis_map <- reactive({
-      create_landkreis_plot(get_location_data(), input$maxFaelleSlider, input$use_oberlab_palette, oberlab_logo, landkreis_logo, input$labelx_map, input$labely_map)
-    })
-
-    #############################################
-    #
-    # Output "Werte über die Zeit" darstellen
-    #
-    #############################################        
-    output$summary_plot <- renderPlot({
-        summary_data <- get_summary_data()$melted
-        if(is.null(summary_data))
-            return(NULL)
-        
-        subset_summary <- subset(summary_data, variable %in% c("Fälle", "Aktive Fälle", "Genesen", "Todesfälle"))
-        
-        create_summary_plot(subset_summary, input$label_summary, oberlab_logo, landkreis_logo, input$fill_it, input$label_it, input$labelx_time, input$labely_time)
-        
-    })
+   
 
     
     #############################################
@@ -190,44 +92,6 @@ shinyServer(function(input, output) {
 # Download Handler
 ###########################################
     
-    #############################################
-    #
-    # Zusammenfassung herunterladen
-    #
-    #############################################
-    
-    output$summary_table_download <- downloadHandler(
-      filename = function(){
-        file <- input$raw_upload
-        name <- file$name
-        gsub(".xlsx", "_ausgewertet.csv", name)
-      },
-      content = function(file){
-        summary <- get_summary_data()$normal
-        if(input$excel_compatible){
-          write_csv2(summary, file)
-        } else {
-          write_csv(summary, file)
-        }
-        
-      })
-    
-    #############################################
-    #
-    # "Werte über die Zeit" herunterladen
-    #
-    #############################################    
-    output$summary_plot_download <- downloadHandler(
-      filename = function(){
-        file <- input$raw_upload
-        name <- file$name
-        gsub(".xlsx", "_summaryplot.png", name)
-      },
-      content = function(file){
-        summary_data <- get_summary_data()$melted
-        subset_summary <- subset(summary_data, variable %in% c("Fälle", "Aktive Fälle", "Genesen", "Todesfälle"))
-        ggsave(create_summary_plot(subset_summary, input$label_summary, oberlab_logo, landkreis_logo, input$fill_it, input$label_it), filename = file, height = 15, width = 30, units = "cm")
-      })
     
     #############################################
     #
